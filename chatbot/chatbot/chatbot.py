8f6aa3fcbc7bd578e0f1c0a4d1d5fa7f82717360
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2024-03-13 18:42:17 -0600

    chat bot

--- chatbot/chatbot/chatbot.py
+++ chatbot/chatbot/chatbot.py
@@ -4,7 +4,7 @@ class KnowledgeBase:
             "Hola": "Hola! En que puedo ayudarte?",
             "Como estas": "Estoy bien, gracias por preguntar. Y tu?",
             "De que te gustaria hablar": "Puedo hablar de muchos temas! Que te interesa?",
-            "Que te gusta hacer?": "Platicar contigo :)",
+            "Porque el cielo es azul": "Por el reflejo del sol"
         }
 
     def get_response(self, input_text):
@@ -12,11 +12,11 @@ class KnowledgeBase:
         if response:
             return response
         else:
-            return "Lo siento, no entendi. Podrias proporcionar mas detalles de la pregunta?"
+            return "Lo siento, no entendi. Podrias proporcionar mas detalles?"
 
     def add_knowledge(self, question, answer):
         self.knowledge[question] = answer
-        return "Se agrego nuevo conocimiento ."
+        return " agregado correctamente."
 
 knowledge_base = KnowledgeBase()
 
@@ -32,8 +32,8 @@ def chat():
             break
         else:
             print("Bot:", response)
-            if response == "Lo siento, no entendi. Podrias darme mas detalles?":
-                new_question = input("Bot: Cual es la respuesta para esta pregunta? ")
+            if response == "Lo siento, no entendi. Podrias proporcionar mas detalles?":
+                new_question = input("Bot: Cual es la pregunta? ")
                 new_answer = input("Bot: Cual es la respuesta a esa pregunta? ")
                 print(knowledge_base.add_knowledge(new_question, new_answer))
 
